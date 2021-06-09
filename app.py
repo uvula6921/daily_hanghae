9516fc2405909d7effccb577786a61283ff960df
kyungjoon.park <pkj6921@naver.com>
2021-06-09 21:50:29 +0900

    Daily_Hanghae - 박경준 로그인, 회원가입 기능 및 login.html, header영역 구현 완료

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
-SECRET_KEY = 'SPARTA'
+SECRET_KEY = 'daily_hanghae'
 
-client = MongoClient('mongodb://test:test@localhost', 27017)
-db = client.dbsparta_plus_week4
+client = MongoClient('15.165.205.11', 27017, username="test", password="test")
+db = client.daily_hanghae
 
 
 @app.route('/')
@@ -52,69 +51,49 @@ def user(username):
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-    # 로그인
-    return jsonify({'result': 'success'})
+    username_receive = request.form["username_give"]
+    password_receive = request.form["password_give"]
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    username_result = db.users.find_one({'username': username_receive})
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+    if username_result is None:
+        msg = "입력하신 아이디가 없습니다."
+        return jsonify({'result': 'fail', 'msg': msg})
+    elif (username_result is not None) & (result is None):
+        msg = "아이디에 맞지 않는 비밀번호입니다. 비밀번호를 재입력해주세요."
+        return jsonify({'result': 'fail', 'msg': msg})
+    else:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24 * 7),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
 
 
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     # 회원가입
     username_receive = request.form['username_give']
+    user_name_receive = request.form['user_name_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    # DB에 저장
+    doc = {
+        'username': username_receive,
+        'user_name': user_name_receive,
+        'password': password_hash
+    }
+    db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
 
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    # ID 중복확인
-    return jsonify({'result': 'success'})
-
-
-@app.route('/update_profile', methods=['POST'])
-def save_img():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 프로필 업데이트
-        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
-
-@app.route('/posting', methods=['POST'])
-def posting():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 포스팅하기
-        return jsonify({"result": "success", 'msg': '포스팅 성공'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
-
-@app.route("/get_posts", methods=['GET'])
-def get_posts():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 포스팅 목록 받아오기
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
-
-@app.route('/update_like', methods=['POST'])
-def update_like():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 좋아요 수 변경
-        return jsonify({"result": "success", 'msg': 'updated'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
